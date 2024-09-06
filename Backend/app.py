95372b1a158ec035c61e75cc0c47336a50833e07
SamidhaManjrekar <smanjrekar135@gmail.com>
2024-09-06 18:27:35 +0530

    Backend done

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+app.config['WTF_CSRF_ENABLED'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+
+import routes 
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
